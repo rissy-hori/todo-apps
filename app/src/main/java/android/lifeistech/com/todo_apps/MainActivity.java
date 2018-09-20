3f03626ede675359d4806b2f21f5465f25f733fa
rissy-hori <shtmt.17kh@gmail.com>
2018-09-20 19:41:54 +0900

    createボタン実装

--- app/src/main/java/android/lifeistech/com/todo_apps/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/todo_apps/MainActivity.java
@@ -1,7 +1,9 @@
 package android.lifeistech.com.todo_apps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void create(View v){
+        Intent intent = new Intent(this, CreateActivity.class);
+        startActivity(intent);
+    }
+
+
 }
